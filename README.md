516775133bce2cf61b0816cd62a26cf220f08e23
xavier madre <xavier.madre@etu.unistra.fr>
2025-03-11 17:39:39 +0100

    preparation for speed test

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 # Exploring-new-long-lived-particles-at-the-LHC-topological-signatures-by-charge-and-lifetime.
 # Exploring-new-long-lived-particles-at-the-LHC-topological-signatures-by-charge-and-lifetime.
 # Read more about SSH config files: https://linux.die.net/man/5/ssh_config
+
+
+
+
+
 Host sbgli2.in2p3.fr
     HostName sbgli2.in2p3.fr
     User xmadre
--- creat_root.py
+++ creat_root.py
@@ -5,47 +5,58 @@ import awkward as ak
 from multiprocessing import Pool
 import time
 import re
-start_time = time.time()
 
-file_list = glob.glob(f"/home/xavier/work/stageM2/code/root_files/*.root")  # creat a list with all root files in the directory
+file_list = glob.glob(f"root_files/*.root")  # creat a list with all root files in the directory
 tree_name = "Ntuple"
 num_cores = 6
 output_file = "output.root"
 
 def root_file(file_name):
-    print(file_name)
     charge_match = re.search(r'Q([0-9.]+)', file_name)
     masse_match = re.search(r'_M([0-9]+)', file_name)
     charge= float(charge_match.group(1))
     masse = float(masse_match.group(1))
-    print(masse ,charge)
     with uproot.open(file_name) as file:
         tree = file[tree_name]
         data = tree.arrays(library="ak")
-        # calculation on Pt 
-        mean_pt=ak.mean(data["Tracks.pt"])
-        median_pt = ak.percentile(data["Tracks.pt"], 50)
-        std_pt= np.sqrt((data["Tracks.pt"]-mean_pt)**2)
+        # calculation 
+        mean=ak.mean(data["Tracks.pt"])
+        std= ak.std(data["Tracks.pt"])
 
-    return mean_pt, median_pt, std_pt, masse, charge
+    return mean, std, masse, charge
 
 
 def multiprocess(file_list):
     with Pool(processes=num_cores) as pool:
         results = pool.map(root_file, file_list)
     mean = [mean for mean in results if mean is not None]
+    std = [std for std in results if std is not None]
+    masse = [masse for masse in results if masse is not None]
+    charge = [charge for charge in results if charge is not None] 
+
 
     indexed_means = np.array(mean)
-    return indexed_means    
+    indexed_std = np.array(std)
+    indexed_mass = np.array(masse)
+    indexed_charge = np.array(charge)
 
-mean = multiprocess(file_list)
-print(mean)
-with uproot.recreate(output_file) as f:
-    f["mean_pt"] = {"mean_tracks_pt": np.array(list(mean), dtype=np.float32),}  
-
+    return indexed_means, indexed_std, indexed_mass, indexed_charge
 
 
 
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"time of calculation : {elapsed_time}s")
+if __name__ == "__main__":
+    start_time = time.time()
+
+
+
+    mean,std,mass,charge = multiprocess(file_list)
+    print(mean)
+    with uproot.recreate(output_file) as f:
+        # f["mean_pt"] = {"mean_pt": np.array(list(mean), dtype=np.float32)}  
+        f["tree"] = { "charge" : charge, "mass": mass, "mean_pt" : mean, "std" :std}
+
+
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"time of calculation : {elapsed_time}s")
